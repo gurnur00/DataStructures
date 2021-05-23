f52c41f99a9efd29081afb944067ed874a833bcb
Gurnur Kaur <gurnurkaur00@gmail.com>
2021-05-23 21:04:45 +0530

    Strings and Arraylist update

--- src/assignment2_arrays/arrayList.java
+++ src/assignment2_arrays/arrayList.java
@@ -1,4 +1,36 @@
 package assignment2_arrays;
 
+import java.util.ArrayList;
+
 public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list + " -> " + list.size());
+
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+        System.out.println(list + " -> " + list.size());
+
+        list.add(1,1000);
+
+        int val = list.get(3);         // cannot use list[1]
+        System.out.println(val);
+
+        list.set(1,2000);          //cannot use list[1] = 2000
+        System.out.println(list + " -> " + list.size());
+
+        list.remove(1);
+        System.out.println(list + " -> " + list.size());
+
+        ArrayList<String> str = new ArrayList<>();
+        str.add("Hi");
+        str.add("Hey");
+        str.add("Heya");
+        str.add("Hello");
+        str.add("Helloz");
+        System.out.println(str + " -> " + str.size());
+    }
 }
--- src/assignment2_arrays/removePrime_arrayList.java
+++ src/assignment2_arrays/removePrime_arrayList.java
@@ -1,4 +1,34 @@
 package assignment2_arrays;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class removePrime_arrayList {
+    public static boolean isPrime(int val){
+        for(int div = 2;div*div<=val;div++){
+            if(val%div == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void solution(ArrayList<Integer> al){
+        // write your code here
+        for(int i = al.size()-1;i>=0;i--){
+            int val = al.get(i);
+            if(isPrime(val)==true){
+                al.remove(i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i = 0 ; i < n; i++){
+            al.add(scn.nextInt());
+        }
+        solution(al);
+        System.out.println(al);
+    }
 }
