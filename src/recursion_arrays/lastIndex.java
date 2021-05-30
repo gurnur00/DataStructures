34e4ec71351e207a59a13f898574075502db78f9
Gurnur Kaur <gurnurkaur00@gmail.com>
2021-05-30 22:33:56 +0530

    Recursion arrays

--- src/recursion_arrays/lastIndex.java
+++ src/recursion_arrays/lastIndex.java
@@ -1,4 +1,31 @@
 package recursion_arrays;
 
+import java.util.Scanner;
+
 public class lastIndex {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner (System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        int x = scn.nextInt();
+        int l = LastIndex(arr,arr.length-1,x);
+        System.out.println(l);
+    }
+
+    public static int LastIndex(int[] arr, int idx, int x){
+        if(idx == 0){
+            return -1;
+        }
+        if(arr[idx]==x){
+            return idx;
+        }
+        else{
+            int li = LastIndex(arr,idx-1,x);
+            return li;
+        }
+    }
 }
