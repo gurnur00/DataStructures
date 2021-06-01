cd135ffa8a021216aa1d12e0b9011210eed4bbbe
Gurnur Kaur <gurnurkaur00@gmail.com>
2021-06-02 00:45:51 +0530

    Stacks

--- src/StackQueue/Balancing_Brackets.java
+++ src/StackQueue/Balancing_Brackets.java
@@ -1,4 +1,59 @@
 package StackQueue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Balancing_Brackets {
+    public static boolean handleClosing(Stack<Character> st, char corres_ch) {
+        if (st.size() == 0) {
+            return false;
+        }
+        else if (st.peek() != corres_ch) {
+            return false;
+        } else {
+            st.pop();
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else if(ch == ')') {
+                boolean val = handleClosing(st, '(');
+                if(val == false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+            else if(ch == '}') {
+                boolean val = handleClosing(st, '{');
+                if(val == false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+            else if(ch == ']') {
+                boolean val = handleClosing(st, '[');
+                if(val == false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+            else{
+
+            }
+        }
+        if(st.size() == 0){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
 }
--- src/StackQueue/intro.java
+++ src/StackQueue/intro.java
@@ -1,4 +1,23 @@
 package StackQueue;
 
+import java.util.Stack;
+
 public class intro {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(10);
+        st.push(20);
+        System.out.println(st);
+        st.push(30);
+        System.out.println(st);
+        st.push(40);
+        System.out.println(st);
+        System.out.println(st.peek()+" "+st.size());
+        st.pop();
+        System.out.println(st.peek()+" "+st.size());
+        st.pop();
+        st.pop();
+        System.out.println(st);
+    }
 }
