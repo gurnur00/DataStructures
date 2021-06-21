cf7acf246061ba6534654a676982f003dd9e4da8
Gurnur Kaur <gurnurkaur00@gmail.com>
2021-06-21 22:18:20 +0530

    new code

--- src/HackerRank/lineNUM.java
+++ src/HackerRank/lineNUM.java
@@ -1,4 +1,12 @@
 package HackerRank;
 
+import java.util.Scanner;
+
 public class lineNUM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1; scan.hasNext()== true; i++){
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
 }
--- src/Walmart/p1.java
+++ src/Walmart/p1.java
@@ -1,4 +1,24 @@
 package Walmart;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class p1 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int X = scn.nextInt();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0;i<X;i++){
+            arr.add(i);
+        }
+        int count = 0;
+        for(int i = X-1; i > 0;i--){
+            if(arr.get(i-1)<=2*arr.get(i)){
+//                arr.remove(i-1);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
